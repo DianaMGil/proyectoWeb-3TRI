a685acfd7618be289e8beb39a4c9ed8e97f7fe1e
David Atienza <david.atienza.casillas@cieep.com>
2024-05-09 11:07:11 +0200

    Importado RouterLink y RouterLinkActivo en home para que funcione el navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormularioComponent } from '../formulario/formulario.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import bootstrap from '../../../main.server';
 // import 'https://fonts.googleapis.com/css?family=Montserrat:400,700';
 // import 'https://fonts.googleapis.com/css?family=Lato:300,400,700');
@@ -11,7 +12,7 @@ import bootstrap from '../../../main.server';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,FormularioComponent,FooterComponent],
+  imports: [NavbarComponent,FormularioComponent,FooterComponent,RouterLink,RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
